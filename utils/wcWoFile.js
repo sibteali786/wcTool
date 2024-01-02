cefa6e1cc0e0908ec7c22f1a41d580e289d3f1d4
Syed Sibteali Baqar <sibteali786@gmail.com>
2024-01-03 02:02:41 +0500

    fixed pielining not working

--- utils/wcWoFile.js
+++ utils/wcWoFile.js
@@ -18,28 +18,30 @@ export function wcWithOutFileName(options) {
     // Calculate bytes for each chunk received
     byteCount += Buffer.byteLength(chunk);
   });
-  if (!inputData) {
-    console.error(
-      chalk.bgRed.red("Please provide a file name or file stream using cat command like cat test.txt | myWc -l"),
-    );
-	process.exit(1);
-  }
-  // When input stream ends (CTRL + D or end of file)
   rl.on("close", () => {
-    if (options.lines) {
-      const result = countLines(inputData);
-      console.log(`${boldRes(result)}`);
-    }
-    if (options.words) {
-      const result = countWords(inputData);
-      console.log(`${boldRes(result)}`);
-    }
-    if (options.characters) {
-      const result = countCharacters(inputData);
-      console.log(`${boldRes(result)}`);
-    }
-    if (options.bytes) {
-      console.log(`${boldRes(byteCount)}`);
+    // readline is async operation
+    if (!inputData) {
+      console.error(
+        chalk.bgRed.red(
+          `Please provide a file name or file stream using cat command like \n${chalk.green.bgWhite(" cat test.txt | myWc -l")}`,
+        ),
+      );
+    } else {
+      if (options.lines) {
+        const result = countLines(inputData);
+        console.log(`${boldRes(result)}`);
+      }
+      if (options.words) {
+        const result = countWords(inputData);
+        console.log(`${boldRes(result)}`);
+      }
+      if (options.characters) {
+        const result = countCharacters(inputData);
+        console.log(`${boldRes(result)}`);
+      }
+      if (options.bytes) {
+        console.log(`${boldRes(byteCount)}`);
+      }
     }
   });
 }
