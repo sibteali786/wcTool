32411972418320349cf3b932a2d82f44c3517588
Syed Sibteali Baqar <sibteali786@gmail.com>
2024-01-04 09:30:10 +0500

    added ability for code to scale

--- utils/wcWoFile.js
+++ utils/wcWoFile.js
@@ -3,45 +3,37 @@ import { countCharacters, countLines, countWords } from "./action.js";
 import { boldRes } from "../utils/beautifyResponse.js";
 import chalk from "chalk";
 
-export function wcWithOutFileName(options) {
+export async function wcWithOutFileName(options) {
   const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
     terminal: false,
   });
-  let inputData = "";
+  let lineCount = 0;
+  let wordCount = 0;
+  let charCount = 0;
   let byteCount = 0;
   rl.on("line", (input) => {
-    inputData += input + "\n";
+    lineCount++;
+    wordCount += countWords(input);
+    charCount += countCharacters(input);
   });
   process.stdin.on("data", (chunk) => {
-    // Calculate bytes for each chunk received
     byteCount += Buffer.byteLength(chunk);
   });
   rl.on("close", () => {
-    // readline is async operation
-    if (!inputData) {
-      console.error(
-        chalk.bgRed.red(
-          `Please provide a file name or file stream using cat command like \n${chalk.green.bgWhite(" cat test.txt | myWc -l")}`,
-        ),
-      );
+    if (options.lines) {
+      console.log(`${boldRes(lineCount)}`);
+    } else if (options.words) {
+      console.log(`${boldRes(wordCount)}`);
+    } else if (options.characters) {
+      console.log(`${boldRes(charCount)}`);
+    } else if (options.bytes) {
+      console.log(`${boldRes(byteCount)}`);
     } else {
-      if (options.lines) {
-        const result = countLines(inputData);
-        console.log(`${boldRes(result)}`);
-      }
-      if (options.words) {
-        const result = countWords(inputData);
-        console.log(`${boldRes(result)}`);
-      }
-      if (options.characters) {
-        const result = countCharacters(inputData);
-        console.log(`${boldRes(result)}`);
-      }
-      if (options.bytes) {
-        console.log(`${boldRes(byteCount)}`);
-      }
+      console.log(
+        `${boldRes(wordCount)} ${boldRes(lineCount)} ${boldRes(byteCount)}`,
+      );
     }
   });
 }
